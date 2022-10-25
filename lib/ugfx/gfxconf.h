48534da0f6f242cb0de4d870b53cc6a8e70889f3
MarkMan0 <pal.mark28@gmail.com>
2022-10-25 14:53:55 +0200

    Change display orientation

--- lib/ugfx/gfxconf.h
+++ lib/ugfx/gfxconf.h
@@ -158,7 +158,7 @@
 //#define GDISP_NEED_PIXMAP                            GFXOFF
 //    #define GDISP_NEED_PIXMAP_IMAGE                  GFXOFF
 
-//#define GDISP_DEFAULT_ORIENTATION                    gOrientationLandscape    // If not defined the native hardware
+#define GDISP_DEFAULT_ORIENTATION                    gOrientation270    // If not defined the native hardware
 //orientation is used. #define GDISP_LINEBUF_SIZE                           128 #define GDISP_STARTUP_COLOR GFX_BLACK
 //#define GDISP_NEED_STARTUP_LOGO                      GFXON
 
