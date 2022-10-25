f6060afb2fc7b59893250150c340e737dcc2d69f
MarkMan0 <pal.mark28@gmail.com>
2022-10-25 22:49:55 +0200

    turn on window manager as per warning

--- lib/ugfx/gfxconf.h
+++ lib/ugfx/gfxconf.h
@@ -195,7 +195,7 @@
 ///////////////////////////////////////////////////////////////////////////
 #define GFX_USE_GWIN                                 GFXON
 
-//#define GWIN_NEED_WINDOWMANAGER                      GFXOFF
+#define GWIN_NEED_WINDOWMANAGER                      GFXOFF
 //    #define GWIN_REDRAW_IMMEDIATE                    GFXOFF
 //    #define GWIN_REDRAW_SINGLEOP                     GFXOFF
 //    #define GWIN_NEED_FLASHING                       GFXOFF
