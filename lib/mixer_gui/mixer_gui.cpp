c5ff07d076ee9ec3fa6c7ddb37488c71caac39b8
MarkMan0 <pal.mark28@gmail.com>
2022-10-25 11:12:13 +0200

    Improved button drawing

--- lib/mixer_gui/mixer_gui.cpp
+++ lib/mixer_gui/mixer_gui.cpp
@@ -1,19 +1,24 @@
 #include "mixer_gui.h"
 #include "mixer_api.h"
 #include "gfx.h"
+#include "src/gwin/gwin_class.h"
+
+static constexpr uint32_t MAX_LINES = 5;
 
 static MixerAPI api;
 
 static void render_volume(const mixer::ProgramVolume&, int line);
 static void clear_line(int line);
-static void destroy_sliders();
+static void init_sliders();
+static void hide_sliders();
+static void show_slider(int line);
 
 
 void mixer_gui_task(ISerial& uart) {
   api.set_uart(&uart);
   gwinSetDefaultStyle(&BlackWidgetStyle, false);
   gwinSetDefaultFont(gdispOpenFont("DejaVuSans12*"));
-
+  init_sliders();
   while (1) {
     if (not api.changes()) {
       vTaskDelay(500);
@@ -24,8 +29,8 @@ void mixer_gui_task(ISerial& uart) {
       vTaskDelay(500);
       continue;
     }
-    destroy_sliders();
     int i = 0;
+    hide_sliders();
     for (const auto& vol : api.get_volumes()) {
       if (vol) {
         render_volume(*vol, i);
@@ -50,30 +55,45 @@ static void clear_line(int line) {
   gdispFillArea(0, base_y + line * multiplier, gdispGetWidth(), multiplier, GFX_BLACK);
 }
 
-GHandle slider_handles[2 * 5] = { 0 };
-static void create_slider(int line) {
-  GWidgetInit wi;
-  gwinWidgetClearInit(&wi);
-  wi.g.show = gTrue;
-  wi.g.x = base_x + multiplier;
-  wi.g.y = base_y + line * multiplier;
-  wi.g.height = 32;
-  wi.g.width = 32;
-  wi.text = "+";
-  slider_handles[2 * line] = gwinButtonCreate(0, &wi);
+static GHandle slider_handles[MAX_LINES][2];
 
-  wi.text = "-";
-  wi.g.x += 40;
-  slider_handles[2 * line + 1] = gwinButtonCreate(0, &wi);
-}
+static void init_sliders() {
+  for (unsigned i = 0; i < MAX_LINES; ++i) {
+    GWidgetInit wi;
+    gwinWidgetClearInit(&wi);
+    wi.g.show = gFalse;
+    wi.g.x = base_x + multiplier;
+    wi.g.y = base_y + i * multiplier;
+    wi.g.height = 32;
+    wi.g.width = 32;
+    wi.text = "+";
+    slider_handles[i][0] = gwinButtonCreate(0, &wi);
 
-static void destroy_sliders() {
-  for (unsigned i = 0; i < sizeof(slider_handles) / sizeof(slider_handles[0]); ++i) {
-    if (slider_handles[i]) {
-      gwinDestroy(slider_handles[i]);
-    }
+    wi.text = "-";
+    wi.g.x += 40;
+    slider_handles[i][1] = gwinButtonCreate(0, &wi);
   }
 }
+
+static void hide_sliders() {
+  for (auto& btns : slider_handles) {
+    btns[0]->flags &= ~(GWIN_FLG_VISIBLE | GWIN_FLG_SYSVISIBLE);
+    btns[0]->flags |= (GWIN_FLG_NEEDREDRAW | GWIN_FLG_BGREDRAW);
+
+    btns[1]->flags &= ~(GWIN_FLG_VISIBLE | GWIN_FLG_SYSVISIBLE);
+    btns[1]->flags |= (GWIN_FLG_NEEDREDRAW | GWIN_FLG_BGREDRAW);
+  }
+}
+
+
+static void show_slider(int line) {
+  if (not utils::within(line, 0, 4)) {
+    return;
+  }
+  gwinShow(slider_handles[line][0]);
+  gwinShow(slider_handles[line][1]);
+}
+
 
 static void render_volume(const mixer::ProgramVolume& vol, int line) {
   gdispImage img;
@@ -87,7 +107,7 @@ static void render_volume(const mixer::ProgramVolume& vol, int line) {
   gdispImageOpenMemory(&img, img_buff);
 
   clear_line(line);
-  create_slider(line);
+  show_slider(line);
   gdispImageDraw(&img, base_x, base_y + line * multiplier, 32, 32, 0, 0);
   gdispImageClose(&img);
 }
